f473c0b4a9bf133b29c27bde1984cceae831c4cb
fumitsuki <fumitsuki@5xruby.tw>
2021-04-06 15:20:07 +0800

    feat: add percent off discount for specified product

--- app/calculators/discount_calculator/percent.rb
+++ app/calculators/discount_calculator/percent.rb
@@ -1,7 +1,16 @@
 class DiscountCalculator
   class Percent < Base
     def calculate
-      (@options * @order.amount * 0.01).to_i
+      return (@options * @order.amount * 0.01).to_i if @options.is_a? Integer
+      return (@options['value'] * order_item.amount * 0.01).to_i if order_item
+
+      0
+    end
+
+    def order_item
+      return unless @options['product_id']
+
+      @order_item ||= @order.order_items.find_by(product_id: @options['product_id'])
     end
   end
 end
